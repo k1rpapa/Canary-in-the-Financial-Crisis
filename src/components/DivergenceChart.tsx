e9949f9f16438ab969edf743bef715e1457d865f
k1rpapa <toru.arima@gmail.com>
2026-08-19 08:32:49 +0900

    fix: remove unused ResponsiveContainer import to fix build failure

--- src/components/DivergenceChart.tsx
+++ src/components/DivergenceChart.tsx
@@ -1,5 +1,5 @@
 
-import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend } from 'recharts';
 
 interface DivergenceChartProps {
   title: string;
